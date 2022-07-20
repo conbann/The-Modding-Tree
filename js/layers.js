7391d0a404778257249d829c6afcffa6ca91f2b0
conbann <conbann@gmail.com>
2022-07-20 19:30:11 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -149,7 +149,7 @@ addLayer("r", {
 addLayer("s", {
     name: "Shards", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "S", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: false,
 		points: new Decimal(0),
@@ -158,10 +158,10 @@ addLayer("s", {
     color: "#FF0000",
     requires: new Decimal(50), // Can be a function that takes requirement increases into account
     resource: "Shards", // Name of prestige currency
-    baseResource: "prestige points", // Name of resource prestige is based on
-    baseAmount() {return player.p.points}, // Get the current amount of baseResource
-    type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 1.25, // Prestige currency exponent
+    baseResource: "rebirth points", // Name of resource prestige is based on
+    baseAmount() {return player.r.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 1, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
@@ -180,7 +180,7 @@ addLayer("s", {
     },
   
     layerShown() {
-        return player.r.unlocked ||hasMilestone("r","11")
+        return player.r.unlocked ||hasMilestone("r","1")
       },
     
 })
