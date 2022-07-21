5ca422ab6af2eadd3dac2fc428cc03e38e8486c1
conbann <conbann@gmail.com>
2022-07-21 12:08:16 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -38,7 +38,7 @@ addLayer("p", {
             description: "Prestige Points boost Points!!",
             cost: new Decimal(3),
             effect() { 
-                return player[this.layer].points.add(1).times(0.3)
+                return player[this.layer].points.add(1).times(0.05)
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
             },
@@ -47,7 +47,7 @@ addLayer("p", {
                 description: "Points boost prestige!",
                 cost: new Decimal(10),
                 effect() { 
-                    return player.points.add(1).times(0.25)
+                    return player.points.add(1).times(0.1)
                 },
                 effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
                 },
@@ -62,7 +62,7 @@ addLayer("p", {
                         description: "points boost points",
                         cost: new Decimal(100),
                         effect() { 
-                            return player.points.add(1).times(0.25)
+                            return player.points.add(1).times(0.1)
                         },
                         unlocked() {return hasMilestone("r","0")},
                         effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" },
