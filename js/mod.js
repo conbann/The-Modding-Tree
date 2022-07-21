1a7fa7b113ab4a4c36b7fd92969bc2ea79895347
conbann <conbann@gmail.com>
2022-07-21 14:55:23 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -64,7 +64,7 @@ var displayThings = [
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.points.gte(new Decimal("e280000000000000000"))
+	return player.s.points.gte(new Decimal("2"))
 }
 
 
