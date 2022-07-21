9acf3d075ccbf3c5e83ae07c326d08beb2b5c51b
conbann <conbann@gmail.com>
2022-07-21 12:09:31 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -62,7 +62,7 @@ addLayer("p", {
                         description: "points boost points",
                         cost: new Decimal(100),
                         effect() { 
-                            return player.points.add(1).times(0.005)
+                            return player.points.add(1).times(1)
                         },
                         unlocked() {return hasMilestone("r","0")},
                         effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" },
